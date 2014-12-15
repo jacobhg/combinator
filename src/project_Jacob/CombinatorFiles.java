b46ac178eabb298198d0d4973586f5e91f497233
uai02 <uai02@localhost>
2014-12-15 11:16:48 +0000

    Actualización (15/12/2014): Se han añadido las clases necesarias para generar el map con los campos de los ficheros .vcf y VEP que necesitamos, y posteriormente generar un nuevo campo INFO que se escribirá en el fichero de salida con toda la información deseada que se ha obtenido de los ficheros de entrada.

--- src/project_Jacob/CombinatorFiles.java
+++ src/project_Jacob/CombinatorFiles.java
@@ -7,6 +7,9 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * ----- Añadido el 10/12/2014 -----
@@ -20,7 +23,7 @@ import java.io.PrintWriter;
 public class CombinatorFiles {
     //----- Añadido el 11/12/2014 -----
     // Vector de string que representa los distintos campos que nos podemos encontrar en el fichero obtenido de VEP:
-    public final String [] vep_fields = {
+    public final String [] vep_fields_long_name = {
         "Gene", "Feature", "Feature_type", "Consequence", "cDNA_position", "CDS_position", "Protein_position", "Amino_acids", "Codons", 
         "DISTANCE", "STRAND", "SYMBOL", "SYMBOL_SOURCE", "ENSP", "SWISSPROT", "TREMBL", "UNIPARC", "HGVSc", "HGVSp", "SIFT", "PolyPhen",
         "MOTIF_NAME", "MOTIF_POS", "HIGH_INF_POS", "MOTIF_SCORE_CHANGE", "CELL_TYPE", "CANONICAL", "CCDS", "INTRON", "EXON", "DOMAINS",
@@ -29,14 +32,14 @@ public class CombinatorFiles {
     
     // Vector que representa las siglas que se han establecido para los diferentes campos que podemos encontrarnos en el 
     // fichero obtenido de VEP:
-    public final String [] vep_fields_tag = {
+    public final String [] vep_fields_short_name = {
         "GENE", "FEAT", "TYPE", "CONS", "CDNA", "CDS", "PROT", "AMINO", "COD", "DIST", "STR", "GNAME", "SRC", "ENSP", "SWPR", "TRBL", "UNI",
         "HGVSc", "HGVSp", "SIFT", "POLY", "MTFN", "MTFP", "HIP", "MSC", "CLLS", "CANON", "CCDS", "INTR", "EXON", "DOM", "IND", "ZYG", "SV",
         "FRQ", "GMAF", "AFR_F", "AMR_F", "ASN_F", "EUR_F", "AA_F", "EA_F", "CLIN", "BIO", "TSL", "PUBM", "SOMA"};
     
     // Vector en el que tenemos las líneas de cabecera correspondientes a los campos del fichero obtenido de VEP y que se
     // incluirán como líneas de cabecera en el fichero de salida:
-    public final String[] vep_info = {
+    public final String[] vep_info_lines = {
         "##INFO=<ID=GENE,Number=1,Type=String,Description=\"Ensemble gene ID\">",
         "##INFO=<ID=FEAT,Number=1,Type=String,Description=\"Ensemble feature ID\">",
         "##INFO=<ID=TYPE,Number=1,Type=String,Description=\"Type of feature (Transcript, RegulatoryFeature, MotifFeature)\">",
@@ -95,6 +98,9 @@ public class CombinatorFiles {
     // Variable string que contendrá la línea que se vaya leyendo del fichero VEP:
     public static String vep_line;
     
+    //----- Añadido el 12/12/2014 -----
+    // Map en el que se guardarán todos los subcampos que se van a añadir al campo INFO del fichero .vcf de salida:
+    public static Map<String, String> info_fields_map;
     
     public CombinatorFiles(){
         
@@ -113,7 +119,7 @@ public class CombinatorFiles {
         File output_file = new File("/home/uai02/Investigacion_Jacob/ficheros_vcf_mist/add_VEP_" + vcf_name);
         
         // Función que añade la información obtenida de VEP al fichero .vcf obtenido de CombinatorVcf:
-        addVep (vcf_file, vep_file, output_file);
+        addVepInfoToVcf (vcf_file, vep_file, output_file);
         
     }
     
@@ -127,7 +133,7 @@ public class CombinatorFiles {
      * @throws IOException
      * @throws FileNotFoundException 
      */
-    private void addVep(File vcf_file, File vep_file, File output_file) throws IOException, FileNotFoundException{
+    private void addVepInfoToVcf(File vcf_file, File vep_file, File output_file) throws IOException, FileNotFoundException{
         // Se lee la primera línea del fichero de entrada .vcf obtenido de CombinatorVcf:
         vcf_br = new BufferedReader(new FileReader(vcf_file));
         vcf_line = vcf_br.readLine();
@@ -135,6 +141,9 @@ public class CombinatorFiles {
         // Se lee la primera línea del fichero de entrada obtenido de VEP (línea de cabecera):
         vep_br = new BufferedReader(new FileReader(vep_file));
         vep_line = vep_br.readLine();
+        //----- Añadido el 12/12/2014 -----
+        // Vector en el que tendremos todas las cabeceras de los campos de VEP:
+        String[] vep_headers = vep_line.split("\t");
         // Se lee la primera línea con datos del fichero de VEP:
         vep_line = vep_br.readLine();
         
@@ -148,13 +157,77 @@ public class CombinatorFiles {
         // la primera línea con datos del fichero .vcf obtenido de CombinatorVcf):
         addVepHeaderToVcf(print_out);
         
+        // Recorrido del fichero de entrada .vcf:
+        while (vcf_line != null){
+            // En un vector almacenamos los campos del fichero .vcf
+            String[] vcf_fields = vcf_line.split("\t");
+            // Recorrido del fichero de entrada VEP:
+            while (vep_line != null){
+                // Almacenamos en un vector los campos del fichero VEP:
+                String[] vep_fields = vep_line.split("\t");
+                // Del campo "Location" (campo 2) del fichero VEP, obtenemos el cromosoma y la posición para poder comparar
+                // con el cromosoma y la posición del .vcf:
+                String[] vep_chrom_pos = vep_fields[1].split(":");
+                // En algunos casos la posición viene indicada como un intervalo de posiciones del tipo "1394663-1394664", por
+                // lo que tendremos que dividir esa línea para quedarnos con la posición que nos interesa.
+                String[] vep_pos = vep_chrom_pos[1].split("-");
+                
+                // Comprobar que el cromosoma y la posición de .vcf y VEP sean la misma:
+                // Caso 1: Si el cromosoma (CHROM) en ambos ficheros coincide, se comprobará la posición (POS) dentro del cromosoma.
+                if (CombinatorVcf.posOfChrom(vcf_fields[0]) == CombinatorVcf.posOfChrom(vep_chrom_pos[0])){ 
+                    // Caso 1.1: Si la posición (POS) del fichero VEP es mayor que la del fichero .vcf, avanzamos una posición en el 
+                    // fichero .vcf (es decir, se lee la siguiente línea del fichero .vcf).
+                    if ((Integer.parseInt(vep_pos[0])) > (Integer.parseInt(vcf_fields[1]))){
+                        break;
+                    }                  
+                    // Caso 1.2: Si la posición (POS) del fichero VEP es menor que la del fichero .vcf, avanzamos una posición en el 
+                    // fichero VEP (es decir, se lee la siguiente línea del fichero VEP).
+                    else if ((Integer.parseInt(vep_pos[0])) < (Integer.parseInt(vcf_fields[1]))){
+                        vep_line = vep_br.readLine();
+                    }                     
+                    // Caso 1.3: Si las posiciones (POS) coinciden, se combina la información de esas líneas (se añade al campo INFO del
+                    // fichero .vcf de salida: el campo INFO del fichero .vcf de entrada y los campos de interés de VEP).
+                    else{
+                        // Se genera el map con los subcampos del campo INFO del fichero .vcf y los campos de interés del fichero VEP:
+                        generateVepVcfMap (vcf_fields, vep_fields, vep_headers);
+                                                
+                        // Se genera la línea que se escribirá en el fichero de salida:
+                        String output_line = vcf_fields[0];
+                        // Agregamos los campos principales del fichero .vcf (todos menos INFO):
+                        for (int i = 1; i < 7; i++){
+                            output_line += "\t" + vcf_fields[i];
+                        }
+                        // Agregamos a la línea el nuevo campo INFO que contiene los campos del .vcf y del VEP:
+                        output_line += "\t" + addToInfoOutput ();
+                        
+                        // Se escribe la línea generada en el fichero de salida:
+                        print_out.println(output_line);
+                        
+                        // Vaciamos el map para utilizarlo en la siguiente iteración: 
+                        info_fields_map.clear();
+                        
+                        // Se lee la siguiente línea en el fichero VEP (se avanza a la siguiente posición (POS)):
+                        vep_line = vep_br.readLine();
+                        break;
+                    }
+                }
+                // Caso 2: Si el cromosoma (CHROM) es diferente en ambos ficheros.
+                else{  
+                    // Caso 2.1: Si el cromosoma (CHROM) del fichero .vcf es menor que el del fichero VEP, avanzamos en el fichero .vcf 
+                    // (se lee la siguiente línea del fichero .vcf).
+                    if (CombinatorVcf.posOfChrom(vcf_fields[0]) < CombinatorVcf.posOfChrom(vep_chrom_pos[0])){
+                        break;
+                    }
+                    // Caso 2.2: Si el cromosoma (CHROM) del fichero .vcf es mayor que el del fichero VEP, avanzamos en el fichero a comparar 
+                    // (se lee la siguiente línea del fichero VEP).
+                    else if (CombinatorVcf.posOfChrom(vcf_fields[0]) > CombinatorVcf.posOfChrom(vep_chrom_pos[0])){
+                        vep_line = vep_br.readLine();
+                    } 
+                }
+            }
+            vcf_line = vcf_br.readLine();
+        }
         
-        
-        // FASE PRINCIPAL: Añadir la información de VEP al campo INFO del .vcf.
-        
-        
-        
-
         // Se cierran los dos ficheros de entrada y el fichero de salida:
         vcf_br.close();
         vep_br.close();
@@ -184,8 +257,8 @@ public class CombinatorFiles {
                     }
                     // Añadimos debajo de las líneas del campo INFO obtenidas del .vcf, las líneas del cabecera del
                     // campo INFO para la información de VEP:
-                    for (int i = 0; i < vep_info.length; i++){
-                        print_out.println(vep_info[i]);
+                    for (int i = 0; i < vep_info_lines.length; i++){
+                        print_out.println(vep_info_lines[i]);
                     }
                 }
                 // Copiamos el resto de líneas de cabecera del fichero .vcf:
@@ -198,5 +271,111 @@ public class CombinatorFiles {
         }
 
     }    
+    
+    /**
+     * ----- Añadido el 12/12/2014 -----
+     * Función que nos indica la posición de un campo del fichero VEP en el vector de string que representa los campos que 
+     * nos interesan de este tipo de ficheros.
+     * @param vep_field_name : Nombre del campo del fichero VEP que queremos comprobar si se encuentra en el listado
+     *                         de campos definidos en el vector "vep_fields_long_name".
+     * @return : Devuelve la posición del vector en la que se encuentra el campo y si no lo ha encontrado devuelve -1.
+     */
+    public int indexOfVepField (String vep_field_name) {
+        for (int i = 0; i < vep_fields_long_name.length; i++) {
+            if (vep_fields_long_name[i].equals(vep_field_name)) {
+                return i;
+            }
+        }
+        return -1;
+    } 
+    
+    /**
+     * ----- Añadido el 12/12/2014 -----
+     * Función que se encargará de generar un map con todos los campos de interés obtenidos del fichero .vcf y del fichero 
+     * VEP, donde la clave del map será el acrónimo que representa el campo y el valor será el valor del propio campo.
+     * @param vcf_fields : Campos obtenidos del fichero .vcf.
+     * @param vep_fields : Capos obtenidos del fichero VEP.
+     * @param vep_headers : Nombre de las cabeceras del fichero VEP.
+     */
+    public void generateVepVcfMap (String[] vcf_fields, String[] vep_fields, String[] vep_headers){
+        // Nos quedamos con los subcampos del campo INFO (campo 8) de los campos pasados por parámetro del fichero .vcf:
+        String[] vcf_info = vcf_fields[7].split(";");
+                        
+        // Map en el que tendremos almacenados los campos que necesitemos de los ficheros .vcf y VEP:
+        info_fields_map = new TreeMap();
+        
+        // PASO 1: Almacenamos en el map los subcampos del campo INFO del fichero .vcf.
+        for (int i = 0; i < vcf_info.length; i++){
+            // Controlamos que se añadan correctamente las etiquetas MistZone:
+            if (vcf_info[i].startsWith("MistZone")){
+                //System.out.println(vcf_info[i]);
+                info_fields_map.put(vcf_info[i], vcf_info[i]);
+            }
+            // Para el resto de los campos:
+            else{
+                //System.out.println(vcf_info[i]);
+                String[] vcf_sub_info = vcf_info[i].split("=");
+                // Controlamos para aquellos casos en los que AF tiene dos valores:
+                if (vcf_sub_info.length == 2){
+                    info_fields_map.put(vcf_sub_info[0], vcf_sub_info[1]);
+                }
+                // Para aquellos casos en los que AF tiene dos valores, concatenamos los valores para que queden ambos
+                // almacenados en el map y separados por coma ",".
+                else if (vcf_sub_info.length == 1){
+                    String af_value = info_fields_map.get("AF");
+                    af_value += "," + vcf_sub_info[0]; 
+                    info_fields_map.put("AF", af_value);
+                }
+            }
+        }
+        
+        // PASO 2: Almacenamos en el map los campos de interés del fichero VEP.
+        for (int i = 0; i < vep_fields.length; i++){
+            // Si el campo se encuentra dentro de la lista de campos que nos interesan:
+            if (indexOfVepField(vep_headers[i]) >= 0){
+                // Comprobamos si existe un valor para ese campo y así evitamos insertar en el map campos que tengan 
+                // valores perdidos (los valores perdidos en el fichero VEP se representan por el símbolo "-"):
+                if (!(vep_fields[i].equals("-"))){
+                    info_fields_map.put(vep_fields_short_name[indexOfVepField(vep_headers[i])], vep_fields[i]);
+                }
+            } 
+        }
+        
+        // Recorrido del map:
+        /*System.out.println("Map con " + info_fields_map.size() + " elementos");
+        for( Iterator it = info_fields_map.keySet().iterator(); it.hasNext();) {
+            String clave = (String)it.next();
+            String valor = (String)info_fields_map.get(clave);
+            System.out.println(clave + " : " + valor);
+        }*/ 
+    }
+    
+    /**
+     * ----- Añadido el 15/12/2014 -----
+     * Función que se utiliza para generar la línea INFO de salida con todos los campos que nos interesan de los ficheros 
+     * .vcf y VEP. 
+     * @return : Devuelve la línea generada.
+     */
+    public String addToInfoOutput (){
+        // Nueva línea de info que se escribirá en el fichero de salida:
+        String output_info_line = "";
+        
+        // Se genera la primera parte de la línea INFO de salida:
+        output_info_line += info_fields_map.keySet().toArray()[0].toString() + "=" + info_fields_map.values().toArray()[0].toString();
+        // Se genera el resto de la línea INFO de salida:
+        for (int i = 1; i < info_fields_map.size(); i++){
+            // Controlamos la etiqueta "MistZone" ya que es la única que no es del tipo: "Etiqueta=Valor":
+            if (info_fields_map.keySet().toArray()[i].toString().equals("MistZone")){
+                output_info_line += ";" + info_fields_map.values().toArray()[i].toString(); 
+            }
+            // Para los campos que no son la etiqueta "MistZone":
+            else {
+                output_info_line += ";" + info_fields_map.keySet().toArray()[i].toString() + "=" + info_fields_map.values().toArray()[i].toString();
+            }
+        }
+        
+        // Se devuelve la línea generada:
+        return output_info_line;
+    }
     
 }
