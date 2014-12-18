9a78516cf301ead5bb3a4c756449225218e66241
uai02 <uai02@localhost>
2014-12-18 13:28:50 +0000

    Actualización (18/12/2014): Nueva clase "CombinatorANNOVAR" con las funciones correspondientes para tratar la información obtenida de un fichero del anotador ANNOVAR.

--- src/combinator/CombinatorANNOVAR.java
+++ src/combinator/CombinatorANNOVAR.java
@@ -3,7 +3,12 @@ package combinator;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.TreeMap;
+import utilities.CombinatorAnnotator;
 
 /**
  * ----- Añadido el 18/12/2014 -----
@@ -91,16 +96,16 @@ public class CombinatorANNOVAR {
         // Fichero de entrada obtenido de CombinatorVcf (en formato .vcf):
         File vcf_file = new File("/home/uai02/Investigacion_Jacob/ficheros_vcf_mist/combinator_ref_niv_084.vcf");
         // Fichero de salida del anotador SIFT en formato .txt:
-        File sift_file = new File("/home/uai02/Investigacion_Jacob/ficheros_vcf_mist/combinator_ref_niv_084_ANNOVAR.csv");
+        File annovar_file = new File("/home/uai02/Investigacion_Jacob/ficheros_vcf_mist/combinator_ref_niv_084_ANNOVAR.csv");
         
         // Fichero de salida (en formato .vcf) en el que tendremos las líneas del fichero obtenido de CombinatorVcf con 
         // la adición de la información de interés del anotador SIFT como subcampos del campo INFO. El fichero también 
         // tendrá sus correspondientes líneas de cabecera:
         String vcf_name = vcf_file.getName();
-        File output_file = new File("/home/uai02/Investigacion_Jacob/ficheros_vcf_mist/add_SIFT_" + vcf_name);
+        File output_file = new File("/home/uai02/Investigacion_Jacob/ficheros_vcf_mist/add_ANNOVAR_" + vcf_name);
         
         // Función que añade la información obtenida de ANNOVAR al fichero .vcf obtenido de CombinatorVcf:
-        addAnnovarToVcf (vcf_file, sift_file, output_file);
+        addAnnovarToVcf (vcf_file, annovar_file, output_file);
         
     }
     
@@ -115,12 +120,198 @@ public class CombinatorANNOVAR {
      * @throws FileNotFoundException 
      */
     private void addAnnovarToVcf(File vcf_file, File annovar_file, File output_file) throws IOException, FileNotFoundException{
+        // Se lee la primera línea del fichero de entrada .vcf obtenido de CombinatorVcf:
+        vcf_br = new BufferedReader(new FileReader(vcf_file));
+        vcf_line = vcf_br.readLine();
         
+        // Se lee la primera línea del fichero de entrada obtenido de ANNOVAR (línea de cabecera con el nombre de los campos):
+        annovar_br = new BufferedReader(new FileReader(annovar_file));
+        annovar_line = annovar_br.readLine();
+        // Vector en el que tendremos todas las cabeceras de los campos de ANNOVAR:
+        String[] annovar_headers = annovar_line.split(",");
+        // Se lee la primera línea con datos del fichero de ANNOVAR:
+        annovar_line = annovar_br.readLine();
         
+        // Fichero de salida en el que tendremos las líneas del fichero .vcf con la adición de los campos de SIFT como 
+        // subcampos del campo INFO:
+        FileWriter output_wr = new FileWriter(output_file);
+        PrintWriter print_out = new PrintWriter(output_wr);
         
+        // Se copian en el fichero de salida las líneas de cabecera del fichero obtenido de CombinatorVcf y se añaden las 
+        // líneas de cabecera correspondientes a los campos de ANNOVAR (al acabar esta función nos encontraremos situados en 
+        // la primera línea con datos del fichero .vcf obtenido de CombinatorVcf):
+        generateOutputHeader(print_out);
+        
+        // Recorrido del fichero de entrada .vcf:
+        while (vcf_line != null){
+            // En un vector almacenamos los campos del fichero .vcf de la línea leída:
+            String[] vcf_fields = vcf_line.split("\t");
+            // Recorrido del fichero de entrada ANNOVAR:
+            while (annovar_line != null){
+                // Almacenamos en un vector los campos del fichero ANNOVAR de la línea leída:
+                String[] annovar_fields = annovar_line.split(",");
+                // Comprobar que el cromosoma y la posición de .vcf y ANNOVAR sean los mismos:
+                // El cromosoma y la posición en el fichero ANNOVAR los tenemos en los dos primeros campos (Chr y Start).
+                // Caso 1: Si el cromosoma (CHROM) de ambos ficheros coincide, se comprobará la posición (POS) dentro del cromosoma.
+                if (CombinatorVcf.posOfChrom(vcf_fields[0]) == CombinatorVcf.posOfChrom(annovar_fields[0])){
+                    // Caso 1.1: Si la posición (POS) del fichero ANNOVAR es mayor que la del fichero .vcf, avanzamos una posición en el 
+                    // fichero .vcf (es decir, se lee la siguiente línea del fichero .vcf).
+                    if ((Integer.parseInt(annovar_fields[1])) > (Integer.parseInt(vcf_fields[1]))){  
+                        break;
+                    }                  
+                    // Caso 1.2: Si la posición (POS) del fichero ANNOVAR es menor que la del fichero .vcf, avanzamos una posición en el 
+                    // fichero ANNOVAR (es decir, se lee la siguiente línea del fichero ANNOVAR).
+                    else if ((Integer.parseInt(annovar_fields[1])) < (Integer.parseInt(vcf_fields[1]))){                        
+                        annovar_line = annovar_br.readLine();
+                    }
+                    // Caso 1.3: Si las posiciones (POS) coinciden, se combina la información de esas líneas (se añade al campo INFO del
+                    // fichero .vcf de salida: el campo INFO del fichero .vcf de entrada y los campos de interés de ANNOVAR).
+                    else{
+                        // Se genera el map con los subcampos del campo INFO del fichero .vcf y los campos de interés del fichero ANNOVAR:
+                        generateAnnovarVcfMap (vcf_fields, annovar_fields, annovar_headers);
+                                                
+                        // Se genera la línea que se escribirá en el fichero de salida:
+                        String output_line = vcf_fields[0];
+                        // Agregamos los campos principales del fichero .vcf (todos menos INFO):
+                        for (int i = 1; i < 7; i++){
+                            output_line += "\t" + vcf_fields[i];
+                        }
+                        // Agregamos a la línea el nuevo campo INFO que contiene los campos del .vcf y del ANNOVAR:
+                        output_line += "\t" + CombinatorAnnotator.generateOutputInfoField ();
+                        
+                        // Se escribe la línea generada en el fichero de salida:
+                        print_out.println(output_line);
+                        
+                        // Vaciamos el map para utilizarlo en la siguiente iteración: 
+                        CombinatorAnnotator.info_fields_map.clear();
+                        
+                        // Se lee la siguiente línea en el fichero ANNOVAR (se avanza a la siguiente posición (POS)):
+                        annovar_line = annovar_br.readLine();
+                        break;
+                    }
+                }
+                // Caso 2: Si el cromosoma (CHROM) es diferente en ambos ficheros.
+                else{  
+                    // Caso 2.1: Si el cromosoma (CHROM) del fichero .vcf es menor que el del fichero ANNOVAR, avanzamos en el fichero .vcf 
+                    // (se lee la siguiente línea del fichero .vcf).
+                    if (CombinatorVcf.posOfChrom(vcf_fields[0]) < CombinatorVcf.posOfChrom(annovar_fields[0])){
+                        break;
+                    }
+                    // Caso 2.2: Si el cromosoma (CHROM) del fichero .vcf es mayor que el del fichero ANNOVAR, avanzamos en el fichero ANNOVAR 
+                    // (se lee la siguiente línea del fichero ANNOVAR).
+                    else if (CombinatorVcf.posOfChrom(vcf_fields[0]) > CombinatorVcf.posOfChrom(annovar_fields[0])){
+                        annovar_line = annovar_br.readLine();
+                    } 
+                }
+            }
+            vcf_line = vcf_br.readLine();
+        }
+        
+        // Se cierran los dos ficheros de entrada y el fichero de salida:
+        vcf_br.close();
+        annovar_br.close();
+        output_wr.close();
+          
     }
     
+    /**
+     * ----- Añadido el 18/12/2014 -----
+     * Función que se utiliza para copiar en el fichero de salida las cabeceras del fichero .vcf y además se añaden las
+     * cabeceras para los campos obtenidos del fichero ANNOVAR.
+     * @param print_out
+     * @throws IOException 
+     */
+    private void generateOutputHeader (PrintWriter print_out) throws IOException {
+        // Se recorre el fichero .vcf:
+        while (vcf_line != null){
+            // Leemos todas las líneas que son de cabecera (las que empiezan por "#"):
+            while (vcf_line.startsWith("#")){
+                // Copiamos las líneas de cabeceras del campo INFO:
+                if (vcf_line.startsWith("##INFO")){
+                    print_out.println(vcf_line);
+                    vcf_line = vcf_br.readLine();
+                    while (vcf_line.startsWith("##INFO")){
+                        print_out.println(vcf_line);
+                        print_out.flush();
+                        vcf_line = vcf_br.readLine();
+                    }
+                    // Añadimos debajo de las líneas de cabecera del campo INFO obtenidas del .vcf, las líneas de cabecera 
+                    // del campo INFO para la información de ANNOVAR:
+                    for (int i = 0; i < annovar_header_lines.length; i++){
+                        print_out.println(annovar_header_lines[i]);
+                        print_out.flush();
+                    }
+                }
+                // Copiamos el resto de líneas de cabecera del fichero .vcf de entrada:
+                else {
+                    print_out.println(vcf_line);
+                    print_out.flush();
+                    vcf_line = vcf_br.readLine();
+                }
+            }
+            break;
+        }  
+    }
     
+    /**
+     * ----- Añadido el 18/12/2014 -----
+     * Función que nos indica la posición de un campo del fichero ANNOVAR en el vector de string que representa los campos que 
+     * nos interesan de este tipo de ficheros.
+     * @param annovar_field_name : Nombre del campo del fichero ANNOVAR que queremos comprobar si se encuentra en el listado
+     *                             de campos definidos en el vector "annovar_fields_long_name".
+     * @return : Devuelve la posición del vector en la que se encuentra el campo y si no lo ha encontrado devuelve -1.
+     */
+    private int indexOfAnnovarField (String annovar_field_name) {
+        for (int i = 0; i < annovar_fields_long_name.length; i++) {
+            if (annovar_fields_long_name[i].equals(annovar_field_name)) {
+                return i;
+            }
+        }
+        return -1;
+    } 
     
+    /**
+     * ----- Añadido el 18/12/2014 -----
+     * Función que se encargará de generar un map con todos los campos de interés obtenidos del fichero .vcf y del fichero 
+     * ANNOVAR, donde la clave del map será el acrónimo o nombre corto que representa el campo y el valor será el valor del 
+     * propio campo.
+     * @param vcf_fields : Campos obtenidos del fichero .vcf de entrada.
+     * @param annovar_fields : Campos obtenidos del fichero ANNOVAR.
+     * @param annovar_headers : Nombre de las cabeceras del fichero ANNOVAR.
+     */
+    private void generateAnnovarVcfMap(String[] vcf_fields, String[] annovar_fields, String[] annovar_headers) {
+        // Nos quedamos con los subcampos del campo INFO (campo 8) de los campos pasados por parámetro del fichero .vcf:
+        String[] vcf_info = vcf_fields[7].split(";");
+                        
+        // Map en el que tendremos almacenados los campos que necesitemos de los ficheros .vcf y ANNOVAR (los campos se 
+        // ordenarán alfabéticamente): 
+        CombinatorAnnotator.info_fields_map = new TreeMap();
+        
+        // PASO 1: Almacenamos en el map los subcampos del campo INFO del fichero .vcf.
+        for (int i = 0; i < vcf_info.length; i++){
+            // Controlamos que se añadan correctamente las etiquetas MistZone, ya que es un campo que no es del tipo
+            // "nombre_del_campo=valor":
+            if (vcf_info[i].startsWith("MistZone")){
+                CombinatorAnnotator.info_fields_map.put(vcf_info[i], vcf_info[i]);
+            }
+            // Para el resto de los subcampos:
+            else{
+                String[] vcf_sub_info = vcf_info[i].split("=");                
+                CombinatorAnnotator.info_fields_map.put(vcf_sub_info[0], vcf_sub_info[1]);
+            }
+        }
+        
+        // PASO 2: Almacenamos en el map los campos de interés del fichero ANNOVAR.
+        for (int i = 0; i < annovar_headers.length; i++){
+            // Si el campo se encuentra dentro de la lista de campos que nos interesan:
+            if (indexOfAnnovarField(annovar_headers[i]) >= 0){
+                // Comprobamos si existe un valor para ese campo y así evitamos insertar en el map campos que tengan 
+                // valores perdidos (los valores perdidos en el fichero ANNOVAR se representan por el símbolo "-" o " "):
+                if (!((annovar_fields[i].equals("")) || (annovar_fields[i].equals(".")) || (annovar_fields[i].equals("\"\"")))){
+                    CombinatorAnnotator.info_fields_map.put(annovar_fields_short_name[indexOfAnnovarField(annovar_headers[i])], annovar_fields[i]); 
+                }
+            } 
+        }   
+    }
     
 }
